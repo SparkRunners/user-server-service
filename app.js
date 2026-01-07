1dd20d46a0e62be42755ebc87187380951043fd8
mats-jonis <majt24@student.bth.se>
2026-01-07 19:44:45 +0100

    Add userRoutes to app.js

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const express = require('express');
+const express = require("express");
 // const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require("./middleware/corsConfig")
+const cors = require("./middleware/corsConfig");
 
 // Require databse constant
-const { connectDB } = require('./db/database');
+const { connectDB } = require("./db/database");
 // Require util constants
 const setupSwagger = require("./utils/swagger");
-
 
 if (process.env.NODE_ENV !== "production") {
   app.use("/coverage", express.static("coverage/lcov-report"));
 }
 
-
-connectDB().catch(err => console.error("DB connect error", err));
+//connectDB().catch((err) => console.error("DB connect error", err));
 //connectDB().catch(err => console.error("DB connect error", err));
 
 // Setup Swagger
 setupSwagger(app);
 
 // Middleware
-// NOTE: 
+// NOTE:
 // If you get cor cross orgin data not allowed error then update in middleware/corConfig.js and add the localhost:PORT trying to acess the apis in user-server-service
 app.use(cors);
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,52 +32,50 @@ const fileLogger = require("./middleware/fileLogger");
 app.use(fileLogger);
 
 // Redefine predefined routes
-const baseRoutes = require('./routes/baseRoutes');
-const scooterRoutes = require('./routes/scooterRoutes');
-const rentRoutes = require('./routes/rentRoutes');
-const zoneRoutes = require('./routes/zoneRoutes');
-const stationRoutes = require('./routes/stationRoutes');
-const citiesRoutes = require('./routes/citiesRoutes');
-const pricingRoutes = require('./routes/pricingRoutes');
+const baseRoutes = require("./routes/baseRoutes");
+const scooterRoutes = require("./routes/scooterRoutes");
+const rentRoutes = require("./routes/rentRoutes");
+const zoneRoutes = require("./routes/zoneRoutes");
+const stationRoutes = require("./routes/stationRoutes");
+const citiesRoutes = require("./routes/citiesRoutes");
+const pricingRoutes = require("./routes/pricingRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // Define routes centraly
-app.use('/', baseRoutes);
-app.use('/api/v1', scooterRoutes);
-app.use('/api/v1/rent', rentRoutes);
-app.use('/api/v1', zoneRoutes);
-app.use('/api/v1', stationRoutes);
-app.use('/api/v1', citiesRoutes);
-app.use('/api/v1', pricingRoutes);
+app.use("/", baseRoutes);
+app.use("/api/v1", scooterRoutes);
+app.use("/api/v1/rent", rentRoutes);
+app.use("/api/v1", zoneRoutes);
+app.use("/api/v1", stationRoutes);
+app.use("/api/v1", citiesRoutes);
+app.use("/api/v1", pricingRoutes);
+app.use("/api/v1", userRoutes);
 
 async function startServer() {
-  // connect to databasea
-
+  // connect to database
   try {
-    await connectDB()
+    await connectDB();
 
-  const Scooter = require('./models/Scooter');
-  const count = await Scooter.countDocuments();
+    const Scooter = require("./models/Scooter");
+    const count = await Scooter.countDocuments();
 
-  if (count == 0) {
-    console.log('Database is empty. Adding mock scooter data.');
-    const scootersData = require('./mock-data/scooters.json');
+    if (count == 0) {
+      console.log("Database is empty. Adding mock scooter data.");
+      const scootersData = require("./mock-data/scooters.json");
+      // eslint-disable-next-line no-unused-vars
+      const scooters = scootersData.map(({ id: _, ...rest }) => rest);
+      await Scooter.insertMany(scooters);
+      console.log(`Added ${scooters.length} scooters.`);
+    }
 
-    // eslint-disable-next-line no-unused-vars
-    const scooters = scootersData.map(({ id: _, ...rest }) => rest);
-    await Scooter.insertMany(scooters);
-    console.log(`Added ${scooters.length} scooters.`);
-  }
-
-  // Start server
-  app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
   } catch (err) {
     console.error("Startup error:", err);
     process.exit(1);
   }
-  
 }
 
 if (process.env.NODE_ENV !== "test") {
